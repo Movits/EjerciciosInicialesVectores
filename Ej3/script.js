d00ad58426a2e409bb1ba4ce6f42df7d56717a58
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-25 14:38:46 -0300

    ej3 finalizado

---
+++ Ej3/script.js
@@ -0,0 +1,11 @@
+const Dias = ["lunes","Martes","Miercules"];
+Dias.splice(1,1);
+console.log(Dias);
+
+const Numeros = [1,2,3];
+Numeros.splice(1,1);
+console.log(Numeros);
+
+const Letras = ["a","b","c"];
+Letras.splice(1,1);
+console.log(Letras);
