7f2fae23af0f1c267120c2ef78552d38ef42389d
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-25 04:18:51 -0300

    ej1v2 finalizado

--- Ej1/script.js
+++ Ej1/script.js
@@ -1,7 +1,10 @@
-const Numeros = ["lunes","Martes","Miercules","jueves","Viernes","Sabado","Domingo"];
+const Dias = ["lunes","Martes","Miercules","jueves","Viernes","Sabado","Domingo"];
+console.log(Dias[0], Dias[3], Dias[6]);
 
-console.log(Numeros[0]);
-console.log(Numeros[3]);
-console.log(Numeros[6]);
+const Numeros = [1,2,3,4,5,6,7];
+console.log(Numeros[0], Numeros[3], Numeros[6]);
 
-console.log(`El vector "Numeros" contiene strings. Un elemento de este vector es: ${Numeros[1]}`);
+const Letras = ["a","b","c","d","e","f","g"];
+console.log(Letras[0], Letras[3], Letras[6]);
+
+console.log(`El vector Numeros contiene numeros. Un elemento de este vector es: ${Numeros[2]}. El vector Dias contiene strings. Un elemento de este vector es: ${Dias[1]}. El vector Letras contiene caracteres. Un elemento de este vector es: ${Letras[0]}.`);
