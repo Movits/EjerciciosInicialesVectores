07b03feb6fead27b375fec0cad5d377c078f7644
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-25 03:59:24 -0300

    ej1v1 finalizado

---
+++ Ej1/script.js
@@ -0,0 +1,7 @@
+const Numeros = ["lunes","Martes","Miercules","jueves","Viernes","Sabado","Domingo"];
+
+console.log(Numeros[0]);
+console.log(Numeros[3]);
+console.log(Numeros[6]);
+
+console.log(`El vector "Numeros" contiene strings. Un elemento de este vector es: ${Numeros[1]}`);
