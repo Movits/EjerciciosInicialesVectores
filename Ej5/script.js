19a69e8324938d9db4d6bb7ed4484f6e8a1a34f1
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-25 22:42:11 -0300

    v4

--- Ej5/script.js
+++ Ej5/script.js
@@ -1,8 +1,11 @@
 const Numeros = [0];
 
 for (let i = 0; i < 29; i++) {
-    Numeros.push((i+1)*2);
-    
+    if (i % 2 === 0) {
+        Numeros.push((i+1)*2);
+    } else {
+        Numeros.push(i+1);
+    }
 }
 
 console.log(Numeros);
