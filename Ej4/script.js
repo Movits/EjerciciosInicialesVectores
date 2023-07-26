360bafd774174c3a66349e8df61f7480d51eea84
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-25 21:39:43 -0300

    ej4 finalizado

---
+++ Ej4/script.js
@@ -0,0 +1,14 @@
+const Dias = ["lunes","Martes"];
+Dias [0] = "Martes";
+Dias [1] = "lunes";
+console.log(Dias);
+
+const Numeros = [1,2];
+Numeros [0] = 2;
+Numeros [1] = 1;
+console.log(Numeros);
+
+const Letras = ["a","b"];
+Letras [0] = "b";
+Letras [1] = "a";
+console.log(Letras);
