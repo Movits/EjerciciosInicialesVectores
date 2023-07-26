141d7c23640e6e4c91798dd523bf48af3099a6b0
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-25 23:54:02 -0300

    ej7v2

--- Ej7/script.js
+++ Ej7/script.js
@@ -1,9 +1,17 @@
 const divContenedorTitulos = document.querySelector("#contenedor-titulos");
 const titulosNum = [1,2,3,4,5,6];
+const titulosContenido = [
+    "Contenido 1",
+    "Contenido 2",
+    "Contenido 3",
+    "Contenido 4",
+    "Contenido 5",
+    "Contenido 6",
+];
 
 for (let i = 0; i < titulosNum.length; i++) {
     divContenedorTitulos.innerHTML+=`
-        <h${titulosNum[i]}>Hola</h${titulosNum[i]}>
+        <h${titulosNum[i]}>${titulosContenido[i]}</h${titulosNum[i]}>
     `;
     
 }
